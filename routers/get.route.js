ab71e854939d5611db532013258c1956c86fd17a
Kennedymuthuri <82339780+Kennedymuthuri@users.noreply.github.com>
2022-11-28 00:25:22 +0300

    Update get.route.js

--- routers/get.route.js
+++ routers/get.route.js
@@ -7,12 +7,9 @@ const router = express.Router();
 const {
     peersGetController,
     blogsGetController,
-    groupsGetController,
     newsGetController,
     mentorsGetController,
     projectGetController,
-    teamsGetController,
-    errorsGetController,
 } = require('../controllers/get.controller');
 
 //Application routes;
@@ -21,17 +18,11 @@ router.get('/peers', peersGetController);
 
 router.get('/blogs', blogsGetController);
 
-router.get('/groups', groupsGetController);
-
 router.get('/news', newsGetController);
 
 router.get('/mentors', mentorsGetController);
 
 router.get('/projects', projectGetController);
-
-router.get('/teams', teamsGetController);
-
-router.get('/errors', errorsGetController);
 
 
 //exporting router;
