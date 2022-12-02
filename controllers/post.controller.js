deb36415cfbcc602b81a6a12e6b0cb9e84283ce6
kennedy muthuri <muthurikennedy082@gmail.com>
2022-12-02 11:53:02 +0300

    debugging

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -99,7 +99,7 @@ const projectPostController = (req, res) => {
             liveLink: req.body.liveLink,
             gitHub: req.body.gitHub,
             image: {
-                data: fs.readFileSync(req.file.filename),
+                data: fs.readFileSync('uploads/' + req.file.filename),
                 contentType: 'image/png'
             }
         });
